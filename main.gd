e321e999f0bdf000f16c0ac4f8f9439414e23643
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-01-12 14:59:56 -0800

    Add the NativLib plugin.

--- main.gd
+++ main.gd
@@ -15,9 +15,39 @@ func _ready() -> void:
             Audio.MAIN_MENU_MUSIC_PLAYER_INDEX, \
             true)
     
-    # FIXME: --------------------------
-    Database.authenticate()
+    login()
 
 func _process(_delta_sec: float) -> void:
     if Input.is_action_just_pressed("screenshot"):
         Utils.take_screenshot()
+
+func login() -> void:
+    facebook.connect( \
+            "login_success", \
+            self, \
+            "_on_facebook_login_success")
+    facebook.connect( \
+            "login_cancelled", \
+            self, \
+            "_on_facebook_login_cancelled")
+    facebook.connect( \
+            "login_failed", \
+            self, \
+            "_on_facebook_login_failed")
+    facebook.login(["email"])
+
+func _on_facebook_login_success(access_token: String) -> void:
+    auth.connect( \
+            "logged_in", \
+            self, \
+            "_on_firebase_login_success")
+    auth.login_facebook(access_token)
+
+func _on_firebase_login_success() -> void:
+    print("Firebase+Facebook login successful!")
+
+# FIXME: Handle failure cases.
+func _on_facebook_login_cancelled() -> void:
+    pass
+func _on_facebook_login_failed() -> void:
+    pass

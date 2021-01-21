329c6a5ea9795744fb8679394d28fa8c79b4dee2
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-01-20 16:47:01 -0800

    Add omitted files from last commit.

---
+++ src/controls/screens/confirm_data_deletion_screen.gd
@@ -0,0 +1,29 @@
+extends Screen
+class_name ConfirmDataDeletionScreen
+
+const TYPE := ScreenType.CONFIRM_DATA_DELETION
+
+func _init().(TYPE) -> void:
+    pass
+
+func _on_ConfirmButton_pressed():
+    Global.give_button_press_feedback()
+    
+    SaveState.erase_all_state()
+    
+    # Erase user files.
+    Utils.clear_directory("user://")
+    
+    var subject := \
+            "?subject=DATA-DELETION REQUEST: Inner-Tube Climber: " + \
+            str(Analytics.client_id)
+    var body := \
+            "&body=(Don't change the subject! The client ID number must " + \
+            "be included for us to know which data to delete.)"
+    OS.shell_open(Constants.SUPPORT_EMAIL_MAILTO + subject + body)
+    
+    get_tree().quit()
+
+func _on_CancelButton_pressed():
+    Global.give_button_press_feedback()
+    Nav.close_current_screen()
